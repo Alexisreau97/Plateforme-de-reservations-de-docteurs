e306d0570c74f5f7e106a4d5c94a68a0784b9e89
Alexisreau97 <alexis.reaupro@gmail.com>
2018-10-25 14:31:36 +0200

    add models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+User.destroy_all
+
+10.times do
+  doctor = Doctor.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, specialty: Faker::Job.field, postal_code: Faker::Address.postcode)
+  patient = Patient.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
+  appointement = Appointement.create!(date: Faker::Time.between(DateTime.now - 1, DateTime.now))
+end
